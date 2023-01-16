75661ddc276065eee7228fca898c19ea3a7f7ec8
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-16 16:46:23 +0500

    Changes made, task completed

--- task8/Program.cs
+++ task8/Program.cs
@@ -6,7 +6,7 @@
 System.Console.Write("Введите число: ");
 int value = Convert.ToInt32(System.Console.ReadLine());
 
-for (int i = 0; i < value; i++) {
+for (int i = 1; i < value; i++) {
     if(i % 2 == 0) {
         System.Console.Write(i + " ");
     }
