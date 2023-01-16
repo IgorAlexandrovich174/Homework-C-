1804fc2087fe076779c4cd366166c6d90a17766d
Igor Alexandrovich Ivanov <igoralexandrovich95@mail.ru>
2023-01-16 16:12:23 +0500

    Completed task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//
+//4 -> да
+//-3 -> нет
+//7 -> нет
+System.Console.Write("Введите число, программа определить его четность: ");
+int value = Convert.ToInt32(System.Console.Read());
+
+if(value % 2 == 0){
+    System.Console.WriteLine("Введенное число четное!");
+} else {
+    System.Console.WriteLine("Введенное число нечетное!");
+}
